eb32924a107ea92bb470db68fb28aaf1f9bc7913
yakkyosan <te21watanabe@g.kumamoto-nct.ac.jp>
2024-11-16 18:25:56 +0900

    QR読み取ると20ポイント追加する （多分無限ループにはならないはず）

--- app/src/main/java/com/example/qr_scanning/viewmodel/QrViewModel.kt
+++ app/src/main/java/com/example/qr_scanning/viewmodel/QrViewModel.kt
@@ -6,13 +6,12 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.qr_scanning.repository.UserRepository
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 
 class QrViewModel(private val userRepository: UserRepository) : ViewModel() {
 
     private val _scanResult = MutableStateFlow<String?>(null)
-    val scanResult: StateFlow<String?> get() = _scanResult
+    // val scanResult: StateFlow<String?> get() = _scanResult
 
     fun addPoints(points: Int) {
         viewModelScope.launch {
